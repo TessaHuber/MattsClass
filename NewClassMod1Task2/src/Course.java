fa570300da1402f1654ca6082778074b177f6a80
tessa <student@MININT-95QZ402>
2016-03-22 16:43:13 -0500

    this is Mod1 Task2

---
+++ NewClassMod1Task2/src/Course.java
@@ -0,0 +1,39 @@
+
+public class Course {
+	private double regnumber;
+	private String dptname;
+	private String address;
+	private String emailID;
+	private int phonenumber;
+	public double getRegnumber() {
+		return regnumber;
+	}
+	public void setRegnumber(double regnumber) {
+		this.regnumber = regnumber;
+	}
+	public String getDptname() {
+		return dptname;
+	}
+	public void setDptname(String dptname) {
+		this.dptname = dptname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmailID() {
+		return emailID;
+	}
+	public void setEmailID(String emailID) {
+		this.emailID = emailID;
+	}
+	public int getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(int phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+
+}
---
+++ NewClassMod1Task2/src/print.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+
+public class print {
+	public static void main(String[] args) {
+		Course C = new Course();
+		
+		Scanner scanner= new Scanner(System.in);
+		double regnumber;
+		System.out.println("please enter student regestraion number:");
+		regnumber=scanner.nextDouble();
+		C.setRegnumber(regnumber); 
+		System.out.println("please enter department name:");
+		String dptname = scanner.nextLine();
+		C.setDptname(dptname);
+		System.out.println("please enter student Permanet address:");
+		String address = scanner.nextLine();
+		C.setAddress(address);
+		System.out.println("please enter student email:");
+		String emailID = scanner.nextLine();
+		C.setEmailID(emailID);
+		System.out.println("please enter student contact phone number:");
+		int phonenumber = scanner.nextInt();
+		C.setPhonenumber(phonenumber);
+		print(C);
+
+	}
+
+	public static void print(Course c) {
+		System.out.println("Student Regestration Number:"+ c.getRegnumber());
+		System.out.println("Department Name:"+c.getDptname() );
+		System.out.println("Student address:"+c.getAddress() );
+		System.out.println("Student Email:"+c.getEmailID() );
+		System.out.println("Student phone number:"+c.getPhonenumber() );
+	}
+
+	
+
+	
+	}
+
