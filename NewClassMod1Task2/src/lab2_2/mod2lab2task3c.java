083a3e49d1788f9ebe5366fe2da0f4295f07ceb6
Matt.Schultz <Matthew.Schultz@consortiex.com>
2016-04-18 14:12:07 -0500

    comment on 2.2 task 3 code -- MS

--- NewClassMod1Task2/src/lab2_2/mod2lab2task3c.java
+++ NewClassMod1Task2/src/lab2_2/mod2lab2task3c.java
@@ -1,8 +1,11 @@
 package lab2_2;
- 
+
 public class mod2lab2task3c {
 	public static void main(String [] args){
 
+		/**
+		 * These should be using arrays, not just print strings, this can be easily converted though, where you have sysouts, set the value of the array[t][k] to the value, then print the arrays out
+		 */
 		for (int t = 0; t <= 10; t++) {
             for (int k = 0; k < 10; k++) {
             System.out.print(" ");
